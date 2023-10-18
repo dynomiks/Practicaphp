3f84261b36e4127ee7c96359b864bcbcd1fcdc94
dynomiks <kameivillcontact@gmail.com>
2023-10-18 10:29:35 -0300

    sql

--- envio.php
+++ envio.php
@@ -22,7 +22,28 @@
 
   echo "</div>";
   echo "<a href=\"index.php\"> Volver a inicio </a>";
-
+  /* crear una tabla*/
 $connection = new SQLite3("estudiantes.db");
+/*me conecto a la tabla*/
+if (!$connection){
+  die("No se pudo conectar")
+}
+/*la creo si es que no hay tabla*/
+$connection->exec(
+  'CREATE TABLE IF NO EXISTS usuario(
+    id INTEGER PRIMARY KEY,
+    nombre TEXT,
+    apellido TEXT,
+    edad TEXT,
+    carrera TEXT
+    )'
+);
+
+$query = "INSERT INTO usuario (nombre,apellido,edad,carrera) VALUES ('$nombre','$apellido','$edad','$carrera')";
+
+$resultado = $connection->exec($query);
+if(!$resultado){
+  echo"Se guardo correctamente!";
+}
 
 ?>
